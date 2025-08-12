9f6317ce90b92513d07040c4924d5a49c63cffb4
PoornaAnalogue <poorna.durga@analogueitsolutionz.com>
2025-08-12 11:29:29 +0530

    new commit

--- .next/server/chunks/ssr/[root-of-the-server]__0fdb80f0._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__0fdb80f0._.js
@@ -14,48 +14,13 @@ __turbopack_context__.n(__turbopack_context__.i("[project]/src/app/favicon.ico.m
 __turbopack_context__.n(__turbopack_context__.i("[project]/src/app/layout.js [app-rsc] (ecmascript)"));
 }),
 "[project]/src/app/page.js [app-rsc] (ecmascript)": ((__turbopack_context__) => {
-"use strict";
 
-__turbopack_context__.s({
-    "default": ()=>App
-});
-var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)");
-(()=>{
-    const e = new Error("Cannot find module './Components/Home/page'");
-    e.code = 'MODULE_NOT_FOUND';
-    throw e;
-})();
-(()=>{
-    const e = new Error("Cannot find module './Components/Services/Android-App-Development/page'");
-    e.code = 'MODULE_NOT_FOUND';
-    throw e;
-})();
-(()=>{
-    const e = new Error("Cannot find module './Components/Testing/page'");
-    e.code = 'MODULE_NOT_FOUND';
-    throw e;
-})();
-;
-;
-;
-;
-function App() {
-    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["Fragment"], {
-        children: [
-            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(Home, {}, void 0, false, {
-                fileName: "[project]/src/app/page.js",
-                lineNumber: 7,
-                columnNumber: 5
-            }, this),
-            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(Services, {}, void 0, false, {
-                fileName: "[project]/src/app/page.js",
-                lineNumber: 9,
-                columnNumber: 5
-            }, this)
-        ]
-    }, void 0, true);
-}
-}),
+var { m: module, e: exports } = __turbopack_context__;
+{
+const e = new Error("Could not parse module '[project]/src/app/page.js'\n\nMerge conflict marker encountered.");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}}),
 "[project]/src/app/page.js [app-rsc] (ecmascript, Next.js Server Component)": ((__turbopack_context__) => {
 
 __turbopack_context__.n(__turbopack_context__.i("[project]/src/app/page.js [app-rsc] (ecmascript)"));
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,45 @@
+ import MainPage from "@/Components/Home/page";
+import IndustryPage from "@/Components/Industries/page";
+import Navbar from "@/Components/Navbar";
+import ProductsPage from "@/Components/Products/page";
+import ProductTesting from "@/Components/Products/producttesting";
+// import ProductTesting from "@/Components/Products/producttesting";
+import HomeTesting from "@/Components/Products/producttesting";
+import ServicePage from "@/Components/Services/page";
+import SingleProductPage from "@/Components/SingleProduct/page";
+import ObjectTechnologiesPagee from "@/Components/Technologies/page";
 import Contact from "./Contact/page";
 import Home from "./Home/page";
 import Services from "./Services/Android-App-Development/page";
-import ServicePage from "./Testing/page";
+// import ServicePage from "./Testing/page";
+import Image from "next/image";
+import MainServicesPage from "./Services/Android-App-Development/page";
+import Footer from "../../Components/Footer";
 
-export default function App(){
-  return (<>
-    <Home />
-    <ServicePage /> 
-    <Services />
-    <Contact />
+export default function App() {
+  return (
+    <>
+      <Home />
+      <ServicePage />
+      <Footer />
 
-    </>)
+
+      <MainServicesPage />
+
+      <ObjectTechnologiesPagee />
+
+      <IndustryPage />
+
+
+      <ProductTesting />
+
+
+      <SingleProductPage /> 
+      
+
+      <Contact />
+      
+
+    </>
+  );
 }
