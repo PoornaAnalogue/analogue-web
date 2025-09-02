db15cfdf80da6e0ae298fc97c1f5a2a26b484501
SikakolliMadhavika <madhavikasikakolli24@gmail.com>
2025-09-02 10:26:41 +0530

    merge

---
+++ src/Components/Footer.js
@@ -0,0 +1,186 @@
+import Image from "next/image";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedinIn,
+  FaWhatsapp,
+  FaYoutube,
+} from "react-icons/fa";
+import { MdAccessTime, MdCall } from "react-icons/md";
+
+export default function Footer() {
+  const socialLinks = [
+  { id: 1, name: "Facebook", icon: FaFacebookF, link: "https://www.facebook.com/analogueitsolutions" },
+  { id: 2, name: "Instagram", icon: FaInstagram, link: "https://www.instagram.com/analogueitsolutions/" },
+  { id: 3, name: "Twitter", icon: FaTwitter, link: "https://x.com/AnalogueIt" },
+  { id: 4, name: "LinkedIn", icon: FaLinkedinIn, link: "https://www.linkedin.com/company/analogueitsolutions/" },
+  { id: 5, name: "Whatsapp", icon: FaWhatsapp, link: "https://api.whatsapp.com/send/?phone=%2B918919088163&text=Hello%2Cbro+edhi+default+message+&type=phone_number&app_absent=0" },
+  { id: 6, name: "Youtube", icon: FaYoutube, link: "https://www.youtube.com/@AnalogueITSolutions" },
+];
+  return (
+    <>
+    <footer className="relative bg-[#0c1a38] text-white overflow-hidden ">
+      {/* Main Content Container */}
+      <div className="max-w-7xl mx-auto px-4 py-12 xss:px-7 xl:px-5 3xl:px-8">
+        {/* Top Section */}
+
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 mb-12">
+        {/* About Us */}
+        <div className="col-span-1 ">
+          <h3 className="font-bold mb-3">About Us</h3>
+          <p className="text-gray-500 leading-relaxed mb-4 text-sm">
+            Analogue is the Best Mobile App Development Company in Hyderabad, India. 
+            We are skilled in Mobile App Development, E-Commerce Development, and Web Development
+          </p>
+          {/* social links */}
+          <div className="flex gap-3 items-center xss:text-xs">
+            {socialLinks.map(({ id, icon: Icon, link, name }) => (
+                <a
+                  key={id}
+                  href={link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label={name}
+                  className="lg:p-2 xss:p-1 bg-[#0e2a5c] rounded-full flex items-center justify-center hover:bg-[#163d7c] transition"
+                >
+                  <Icon className="text-white" size={18} />
+                </a>
+              ))}
+          </div>
+        </div>
+
+        {/* Links and Our Services Container */}
+        <div className="col-span-1 xl:col-span-2">
+          <div className="grid grid-cols-2 gap-6">
+            {/* Links */}
+            <div className="xl:ml-18">
+              <h3 className="font-bold mb-3">Links</h3>
+              <ul className="space-y-2 text-gray-500 text-sm">
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Our Mission</a></li>
+                <li><a href="#">Landing Page</a></li>
+                <li><a href="#">Our Projects</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+
+            {/* Our Services */}
+            <div>
+              <h3 className="font-bold mb-3">Our Services</h3>
+              <ul className="space-y-2 text-gray-500 text-sm">
+                <li>Ui Ux Development</li>
+                <li>Web Development</li>
+                <li>Mobile App Development</li>
+                <li>Digital Marketing</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+
+
+        {/* Location */}
+        <div className="col-span-1 xl:col-span-1">
+          <h3 className="font-bold mb-3">Location</h3>
+          <div className="grid grid-cols-1">
+            <div className="flex items-center gap-2 lg:p-1 lg:min-w-[100px] ">
+              <span className="text-gray-500 lg:text-sm xss:text-lg">Hyderabad, India</span>
+              <Image src="/Flags/India-Flag.svg" width={20} height={20} alt="India" className="w-auto h-auto max-w-[25px] max-h-[16px]" />
+            </div>
+            <div className="flex items-center gap-2 p-1 min-w-[100px]">
+              <span className="text-gray-500 lg:text-sm xss:text-lg">Dubai</span>
+              <Image src="/Flags/Uae-Flag.svg" width={28} height={20} alt="UAE" className="w-[28px]  h-[20px]" />
+            </div>
+            <div className="flex items-center gap-2 p-1 min-w-[100px]">
+              <span className="text-gray-500 lg:text-sm xss:text-lg">California</span>
+              <Image src="/Flags/California-republic.svg" width={28} height={14} alt="USA" className="w-auto h-auto max-w-[28px] max-h-[25px]" />
+            </div>
+            <div className="flex items-center gap-2 p-1 min-w-[100px]">
+              <span className="text-gray-500 lg:text-sm xss:text-lg">Denmark</span>
+              <Image src="/Flags/Denmark-Flag.svg" width={28} height={14} alt="Denmark" className="w-auto h-auto max-w-[28px] max-h-[24px]" />
+            </div>
+            <div className="flex items-center gap-2 p-1 min-w-[100px]">
+              <span className="text-gray-500 lg:text-sm xss:text-lg">Qatar</span>
+              <Image src="/Flags/Qatar-Flag.svg" width={28} height={14} alt="Qatar" className="w-[28px]  h-[14px]" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+              {/* Middle Section */}
+      <div className="grid grid-cols-1 xl:grid-cols-4 gap-6 xl:px-10 2xl:px-7">
+        {/* Copyright */}
+        <div className="text-start text-sm text-gray-200 xl:mx-[-1.5rem] hidden xl:block">
+          Copyright© 2023 Tronix. All Rights Reserved.
+        </div>
+
+        {/* address, working hours, contact Details */}
+        <div className="col-span-3 grid grid-cols-1 xl:grid-cols-3 xss:gap-4 xl:gap-2 2xl:gap-5 xl:ml-[-1rem] 2xl:ml-0">
+  {/* Location */}
+  <div className="flex items-start gap-2">
+    <div className="relative w-[50px] h-[50px] xss:w-[150px] xss:h-[50px] sm:w-[130px] sm:ml-[-0.8rem] md:ml-[-0.15rem] md:w-[4.5rem] md:h-[4rem] xs:ml-[-0.4rem] sm:h-[60px] xl:w-[250px] xl:h-[60px] xl:ml-0 3xl:w-[180px]">
+      <Image src="/Footer/Location.svg" alt="Location" fill className="object-contain" />
+    </div>
+    <div className="flex flex-col">
+      <h2 className="xss:text-xs sm:text-md lg:text-[0.9rem] xl:text-md">LOCATION</h2>
+      <p className="text-gray-500 text-sm mt-2">
+        <a
+          href="https://maps.app.goo.gl/P858gAyx5vH8GaTQ7?g_st=ac"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Open location in Google Maps"
+        >
+          1-98/4/64, Jaya Vijaya Plaza, Vittal Rao Nagar, Mindspace, Madhapur, Hyderabad-500081.
+        </a>
+      </p>
+    </div>
+  </div>
+
+  {/* Working Hours */}
+  <div className="flex items-start gap-4">
+    <div className="relative w-[50px] h-[50px] xss:w-[50px] xss:h-[50px] sm:w-[60px] sm:h-[60px]">
+      <Image src="/Footer/Working-Hours.svg" alt="Working Hours" fill className="object-contain" />
+    </div>
+    <div className="flex flex-col">
+      <h2 className="xss:text-xs sm:text-md lg:text-[0.9rem] xl:text-md">WORKING HOURS</h2>
+      <p className="text-gray-500 text-sm mt-2">
+        Monday - Saturday <br /> 10am - 7pm
+      </p>
+    </div>
+  </div>
+
+  {/* Contact Us */}
+  <div className="flex items-start gap-4 xl:w-[15.5rem] 2xl:ml-[-1.2rem]">
+    <div className="relative w-[50px] h-[50px] xss:w-[50px] xss:h-[80px] sm:w-[60px] sm:h-[60px] xl:w-[250px] xl:h-[60px] 3xl:w-[200px]">
+      <Image src="/Footer/Contact.svg" alt="Contact" fill className="object-contain" />
+    </div>
+    <div className="flex flex-col">
+      <h2 className="xss:text-xs sm:text-md lg:text-[0.9rem] xl:text-md">CONTACT US</h2>
+      <a href="mailto:info@analogueitsolutions.com" className="hover:underline text-gray-500 text-sm">
+        info@analogueitsolutions.com
+      </a>
+      <a href="tel:+918919088163" className="hover:underline text-gray-500 text-sm">
+        +91 89190 88163
+      </a>
+    </div>
+  </div>
+</div>
+
+
+      </div>
+      </div>
+
+      {/* Background Big Text */}
+      <div className="2xl:mt-45 xl:mt-25">
+        <h1 className="absolute xss:hidden xl:block 3xl:bottom-[-78px] 3xl:text-[16rem] 2xl:bottom-[-65px] 2xl:text-[14rem] xl:bottom-[-52px] xl:text-[11rem] left-0 right-0 font-semibold text-[#103687] leading-none z-0 select-none text-center">
+          Analogue IT
+        </h1>
+      </div>
+        <div className="text-center md:text-lg xss:px-3 xss:text-sm bg-[#1a265c] py-4 w-screen text-gray-200 mt-4 xl:hidden">
+    Copyright © 2025 Analogue IT Solutions. All Rights Reserved.
+  </div>
+    </footer>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,108 +1,10 @@
-import Image from "next/image";
-import { Scroll } from "lucide-react";
-
-import Home from "./Home/page";
-import Footer from "../components/Footer";
-import Guarantees from "./guarantees/page";
-import AboutUs from "./aboutus/page";
-import GetAQuoteForm from "../../Components/GetAQuoteForm";
-import Navbar from "../../Components/Navbar";
-import Responsive_Best_App from "./BestMobileApp/page";
-import Responsive_ServicePage from "./ServicesPage/page";
-import Responsive_Testimonials from "./TestimonialsPage/page";
-import Responsive_India from "./India/page";
-import PuzzleTest from "./testclient/page";
-import Responsive_ServicePage_Mobile from "./ServicesPage/page";
-import Responsive_Calendar from "./BookAppointment/page";
-import PuzzleScrollSection from "./puzzlescroll/page";
-
-
-import Responsive_AngularTech from "./Tech-Angular/page";
-import Responsive_AWSTech from "./Tech-AWS/page";
-import Responsive_PyTech from "./Tech-Py/page";
-import Responsive_FigmaTech from "./Tech-Figma/page";
-import Responsive_AzureTech from "./Tech-Azure/page";
-import Responsive_NodeTech from "./Tech-Node/page";
-import Responsive_MongoTech from "./Tech-Mongo/page";
-import Responsive_OracleTech from "./Tech-Oracle/page";
-import Responsive_WebTech from "./Tech-Web/page";
-import Responsive_PhotoShopTech from "./Tech-Photoshop/page";
-import Responsive_JavaTech from "./Tech-Java/page";
-import Responsive_GCPTech from "./Tech-GCP/page";
-import Responsive_NextTech from "./Tech-Next/page";
-import Responsive_ReactNTech from "./Tech-ReactN/page";
-import Responsive_PHPTech from "./Tech-PHP/page";
-import Responsive_VueJsTech from "./Tech-Vue/page";
-// import Responsive_SingleProductPage from "@/app/SingleProduct/page";
-import Responsive_IndusNews from "./Indus-News/page";
-import Responsive_IndusWeb from "./Indus-News copy/page";
-import Responsive_ProductsPage from "./Products/page";
-
-import Contact from "./Contact/page";
-import AndroidAppDevelopment from "./Services/android-app-development-company-in-hyderabad/page";
-import Responsive_AdobeTech from "./Tech-Adobe/page";
-// import Navbar from "../../Components/Navbar";
-import Responsive_SingleProductPage from "./SingleProduct/page";
-import ClientCarousel from "./PoornaCarousal/page";
 import MainHomePage from "@/app/MainHomePage/page";
 
 export default function App() {
   return (
     <>
-
       
-      {/* <AboutUs /> */}
-      <Home/>
-      <Responsive_ServicePage_Mobile />
-      <ClientCarousel />
-      <Guarantees />
-      <PuzzleScrollSection/>
-      <Responsive_Best_App />
-      <Responsive_Testimonials />
-      <Responsive_Calendar />
-      <Responsive_India />
-      <GetAQuoteForm />
-      <Footer/>
-
-
-
-
-      {/* <Responsive_SingleProductPage /> */}
-      {/* <Responsive_AngularTech /> */}
-      {/* <Responsive_AdobeTech /> */}
-
-      {/* <Responsive_IndusNews /> */}
-      {/* <AndroidAppDevelopment /> */}
-
-        {/* 
-       
-      
-      
-      
-      <Responsive_ProductsPage />
-      <Contact /> 
-      */}
-      
-    {/* 
-      <Responsive_AdobeTech />
-      <Responsive_AngularTech />
-      <Responsive_AWSTech />
-      <Responsive_PyTech />
-      <Responsive_FigmaTech />
-      <Responsive_AzureTech />
-      <Responsive_NodeTech />
-      <Responsive_MongoTech />
-      <Responsive_OracleTech />
-      <Responsive_WebTech />
-      <Responsive_PhotoShopTech />
-      <Responsive_JavaTech />
-      <Responsive_GCPTech />
-      <Responsive_NextTech />
-      <Responsive_ReactNTech />
-      <Responsive_PHPTech />
-      <Responsive_VueJsTech />
-      <Responsive_IndusWeb /> 
-    */}
+      <MainHomePage />
 
     </>
   );
