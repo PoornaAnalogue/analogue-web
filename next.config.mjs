14a1c8c0e9c94a1886cabed23efec85f2180d3ab
Suryamurugesan55 <mssuryamurugesan@gmail.com>
2025-09-18 16:14:06 +0530

    Revised rebase

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {
     images: {
     unoptimized: true,
   },
 };
  
-=======
-const nextConfig = {};
-
->>>>>>> 07fd8890ed6014623b403ff8fbe98eb7f97a02d1
 export default nextConfig;
